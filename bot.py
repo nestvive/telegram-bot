32fde1a779d4f5c9c5c04b1ff455a1735e34b1d5
nestvive <1175932@qq.com>
2025-03-03 20:48:38 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,8 +11,9 @@ def home():
 def webhook():
     update = request.json
     print(update)  # 打印收到的数据
-    return "OK"，200
+    return "OK", 200
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))  # 确保使用 Railway 的端口
     app.run(host='0.0.0.0', port=port, debug=True)
+
